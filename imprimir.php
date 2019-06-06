6c7ab1affa72a7227ef186241eab8da00956a82a
facundo martin <facundo.martin.9@gmail.com>
2019-06-06 19:03:43 -0300

    Registración

---
+++ imprimir.php
@@ -0,0 +1,50 @@
+<?php
+$error_mail=false;
+$error_nombre=false;
+if(($_POST["nombre"])=="" || ($_POST["nombre"])==null ){
+  $error_nombre=true;
+}
+if(($_POST["email"])=="" || ($_POST["email"])==null || !(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL))){
+  $error_mail=true;
+}
+echo "<br>";
+if($error_mail==true){
+echo "Mail inválido.";
+}
+echo "<br>";
+if($error_nombre==true){
+echo "Nombre vacío.";
+}
+if ($error_mail==true || $error_nombre==true){
+
+}
+echo "<br>";
+if ($error_mail==false && $error_nombre==false){
+echo "Correcto";
+$almacenar="{Email: ". $_POST["email"] .", Nombre: ".$_POST["nombre"]."}\n";
+header('Location: http://127.0.0.1/practicaDH/php_clase_06_04/happy.html');
+file_put_contents("prueba.txt",file_get_contents("almacenamiento.txt").$almacenar);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+
+<body>
+<!--<form action="imprimir.php" method="get">-->
+<form action="imprimir.php" method="POST">
+<label for="nombre">Nombre:</label>
+<input type="text" name="nombre">
+<br>
+<label for="email">E-mail:</label>
+<input type="text" name="email">
+<br>
+<input type="submit">
+</form>
+</body>
+</html>
